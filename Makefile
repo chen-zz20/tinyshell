0494576b6f1b5c85c3f7a698693df5df63357c0e
Chen Zezhou <chen-zz20@mails.tsinghua.edu.cn>
2023-05-15 22:29:52 +0800

    帮我的宝贝修改代码

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 all: main
 
 echo.o: echo.cpp
-	g++ -c $^ -o $@
+	g++ -c $^ -o $@ -static
 
 main.o:main.cpp
-	g++ -c $^ -o $@
+	g++ -c $^ -o $@ -static
 
 main: main.o echo.o
-	g++ $^ -o main.exe
+	g++ $^ -o main.exe -static
 
 clean:
 	del *.o *.exe
--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ void initial(){
     cout << "Login: ";
     cin >> gTerm.user;
     gTerm.wdir = "/";
-    cout << gTerm.user << "@" << gTerm.mach << ":" << gTerm.wdir << "$ ";
+    cin.get();
 }
 
 
@@ -34,62 +34,66 @@ void initial(){
 
 
 int main(int argc, const char * argv[]) {
-    //initial();
-    string user_input;
-    vector<string> substrings;
-    getline(cin, user_input);
-    size_t pos = 0;
-    while ((pos = user_input.find("|")) != string::npos) {
-        string substring = user_input.substr(0, pos);
-        substrings.push_back(substring);
-        user_input.erase(0, pos + 1);
-    }
-    substrings.push_back(user_input);   //分割复合指令
-    vector<vector<string>> split_substrings;
-        for (const auto& substring : substrings) {
-            vector<string> split;
-            istringstream iss(substring);
-            string word;
-            while (iss >> word) {
-                //cout<<"word:  "<<word<<endl;
-                split.push_back(word);
+    initial();
+    while(true){
+        gTerm.strin = "";
+        gTerm.strout = "";
+        cout << gTerm.user << "@" << gTerm.mach << ":" << gTerm.wdir << "$ ";
+        string user_input;
+        vector<string> substrings;
+        getline(cin, user_input);
+        size_t pos = 0;
+        while ((pos = user_input.find("|")) != string::npos) {
+            string substring = user_input.substr(0, pos);
+            substrings.push_back(substring);
+            user_input.erase(0, pos + 1);
+        }
+        substrings.push_back(user_input);   //分割复合指令
+        vector<vector<string>> split_substrings;
+            for (const auto& substring : substrings) {
+                vector<string> split;
+                istringstream iss(substring);
+                string word;
+                while (iss >> word) {
+                    //cout<<"word:  "<<word<<endl;
+                    split.push_back(word);
+                }
+                split_substrings.push_back(split);
+            }   //分割每个指令字符串
+        for(auto order:split_substrings){
+            //cout<<order[0]<<" "<<order[1]<<endl;
+            /*if (order[0].compare("diff") == 0) {
+                doDiff(argc, argv);
             }
-            split_substrings.push_back(split);
-        }   //分割每个指令字符串
-    for(auto order:split_substrings){
-        //cout<<order[0]<<" "<<order[1]<<endl;
-        /*if (order[0].compare("diff") == 0) {
-            doDiff(argc, argv);
+            else if (order[0].compare("grep") == 0) {
+                doGrep(argc, argv);
+            }
+            else if (order[0].compare("tee") == 0) {
+                doTee(argc, argv);
+            }
+            else if (order[0].compare("cat") == 0) {
+                doCat(argc, argv);
+            }
+            else if (order[0].compare("cp") == 0) {
+                doCp(argc, argv);
+            }
+            else if (order[0].compare("cd") == 0) {
+                doCd(argc, argv);
+            }
+            else if (order[0].compare("pwd") == 0) {
+                doPwd(argc, argv);
+            }*/
+            if (order[0] == "echo") {
+                doEcho(order);
+            }
+            else if (order[0] == "exit") {
+                return 0;
+            }
+            else {
+                //Red("Error: ");
+                cerr << "Command '" << order[0] << "' not found" << endl;
+            }
         }
-        else if (order[0].compare("grep") == 0) {
-            doGrep(argc, argv);
-        }
-        else if (order[0].compare("tee") == 0) {
-            doTee(argc, argv);
-        }
-        else if (order[0].compare("cat") == 0) {
-            doCat(argc, argv);
-        }
-        else if (order[0].compare("cp") == 0) {
-            doCp(argc, argv);
-        }
-        else if (order[0].compare("cd") == 0) {
-            doCd(argc, argv);
-        }
-        else if (order[0].compare("pwd") == 0) {
-            doPwd(argc, argv);
-        }*/
-        if (order[0] == "echo") {
-            doEcho(order);
-        }
-        else if (order[0] == "exit") {
-            return 0;
-        }
-        else {
-            //Red("Error: ");
-            cout << "Command '" << order[0] << "' not found" << endl;
-        }
-
     }
     return 0;
 }
